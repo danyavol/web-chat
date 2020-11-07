1cc58eacf52ceca3b32eef3b083b96e2f6524aee
Volosyuk Daniil <danya.volosyuk@gmail.com>
2020-11-07 19:59:36 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		beforeCreate() {
+			// Проверка авторизован ли пользователь
+			if ( this.$router.currentRoute.fullPath === '/') {
+				if ( localStorage.getItem('isAuth') ) {
+					this.$router.push('/auth');
+				}
+			}
+		},
+		components: {
+
+		}
+	}
+
+
+</script>
